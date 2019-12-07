21e1d4724aa22ae78abe4ce1bc6b910cec70c115
Filipe Felice Rocha da Silva <filipefelice@gmail.com>
2019-12-06 23:10:42 -0200

    teste de ci

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js
+          Welcome to New School Project
         </v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
