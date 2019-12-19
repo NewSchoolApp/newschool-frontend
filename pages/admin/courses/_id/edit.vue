14762aa90b92ddfec4e9ba44d29e8008ee7522bd
Carlos Prado <carlos.prado@portalsolar.com.br>
2019-12-19 18:23:50 -0300

    Add edit course page

---
+++ pages/admin/courses/_id/edit.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-layout justify-center>
+    <v-flex ref="flex" xs10 sm8 md4>
+      <h1>Editando curso</h1>
+      <v-form>
+        <v-text-field
+          :value="course.title"
+          label="Título"
+          required
+        />
+        <v-textarea
+          :value="course.description"
+          label="Descrição"
+          required
+        />
+        <v-text-field
+          :value="course.author"
+          label="Título"
+          required
+        />
+        <v-textarea
+          :value="course.authorInfo"
+          label="Descrição"
+          required
+        />
+      </v-form>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    computed: {
+      course() {
+        return this.$store.state.courses.current
+      }
+    },
+    asyncData({ store, data, params, $axios }) {
+      return $axios.get(`http://localhost:3030/api/v1/courses/${params.id}`)
+        .then(res =>
+          store.commit('courses/setCurrent', res.data.course)
+        )
+    }
+  }
+</script>
+
+<style scoped>
+h1 {
+  font-family: Montserrat;
+  font-weight: 900;
+  font-size: 20px;
+  line-height: 24px;
+  text-transform: uppercase;
+
+  color: #6600CC;
+}
+
+</style>
--- store/courses.js
+++ store/courses.js
@@ -1,9 +1,13 @@
 export const state = () => ({
-  list: []
+  list: [],
+  current: null
 })
 
 export const mutations = {
   set (state, courses) {
-    state.list.push(...courses)
+    state.list = courses
+  },
+  setCurrent (state, course) {
+    state.current = course
   }
 }
