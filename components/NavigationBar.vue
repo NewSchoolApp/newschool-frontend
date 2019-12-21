c8c5661c8a8523a7609b22188a8583884530ca38
Felipe Andrew <ferulip@gmail.com>
2019-12-21 15:16:07 -0300

    Navigation-menu e seus itens

---
+++ components/NavigationBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <v-bottom-navigation
+      scroll-target="#scroll-area-2"
+      hide-on-scroll
+      scroll-threshold="500"
+      absolute
+      color="write"
+      horizontal
+    >
+      <v-btn>
+        <span>Home</span>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+      <v-btn>
+        <span>Perfil</span>
+        <v-icon>mdi-account-outline</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Meus Cursos</span>
+        <v-icon>mdi-library</v-icon>
+      </v-btn>
+      <v-btn>
+        <span>Certificados</span>
+        <v-icon>mdi-school</v-icon>
+      </v-btn>
+      <v-btn>
+        <span>Outros</span>
+        <v-icon>mdi-drag</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+
+    <v-sheet id="scroll-area-2" class="overflow-y-auto" max-height="600">
+      <v-container style="height: 1500px;"></v-container>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.v-btn__content {
+  flex-direction: column-reverse !important;
+}
+.v-icon {
+  margin-right: 0 !important;
+}
+</style>
