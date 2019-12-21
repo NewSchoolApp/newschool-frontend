61fad7ecf82b1d517bd7c4309864d64cba427864
Felipe Andrew <ferulip@gmail.com>
2019-12-21 13:42:56 -0300

    padronizando cor principal da aplicação no config

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,7 @@ export default {
       dark: false,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
+          primary: '#6600cc',
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
