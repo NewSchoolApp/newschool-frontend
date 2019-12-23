d5953837a59669ce918e90cd747914d2fc0a10eb
Felipe Andrew <ferulip@gmail.com>
2019-12-23 17:13:22 -0300

    atualizando login page UI/UX, ajuste dos ícones do navigation-menu, ajuste de cor padrõa light-theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,6 +81,9 @@ export default {
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
+        },
+        light: {
+          primary: '#6600cc',
         }
       }
     }
