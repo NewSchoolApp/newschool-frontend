8f0538d8491c4a76697fb97ae215901652dfdc39
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-12-23 08:49:20 -0300

    Adiciona as aulas na pagina de ediçao

--- pages/admin/courses/_id/edit.vue
+++ pages/admin/courses/_id/edit.vue
@@ -23,7 +23,22 @@
           label="Descrição"
           required
         />
+        <v-btn color="primary">Salvar</v-btn>
       </v-form>
+
+      <div class="classes">
+        <h2>Aulas</h2>
+        <div v-for="klass in course.classes" v-bind:key="klass.id" class="class">
+          <div class="class__info">
+            <p class="class__title">{{ klass.title }}</p>
+          </div>
+          <div class="actions">
+            <nuxt-link v-bind:to="editLink(klass)">Editar</nuxt-link>
+            <nuxt-link v-bind:to="deleteLink(klass)">Excluir</nuxt-link>
+          </div>
+       </div>
+      </div>
+
     </v-flex>
   </v-layout>
 </template>
@@ -36,17 +51,27 @@
       }
     },
     asyncData({ store, data, params, $axios }) {
-      return $axios.get(`http://localhost:3030/api/v1/courses/${params.id}`)
-        .then(res =>
-          store.commit('courses/setCurrent', res.data.course)
-        )
+      const coursePromise = $axios.get(
+        `http://localhost:3030/api/v1/courses/${params.id}`
+      ).then(res =>
+        store.commit('courses/setCurrent', res.data.course)
+      )
+      const classesPromise = $axios.get(
+        `http://localhost:3030/api/v1/courses/${params.id}/classes`
+      ).then(res =>
+        store.commit('courses/setCurrentClasses', res.data.classes)
+      )
+      return Promise.all([coursePromise, classesPromise])
+    },
+    methods: {
+      editLink: klass => `classes/${klass.id}/edit`,
+      deleteLink: klass => `classes/${klass.id}/delete`
     }
   }
 </script>
 
 <style scoped>
 h1 {
-  font-family: Montserrat;
   font-weight: 900;
   font-size: 20px;
   line-height: 24px;
@@ -55,4 +80,49 @@ h1 {
   color: #6600CC;
 }
 
+h2 {
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 24px;
+  text-transform: uppercase;
+
+  color: #6600CC;
+}
+
+.classes {
+  margin-top: 20px;
+  padding: 20px 0;
+  border-top: 1px solid #e3e0e6;
+}
+
+
+.class {
+  border-bottom: 1px solid #e3e0e6;
+  padding: 10px 0;
+  display: flex;
+  justify-content: space-between;
+}
+
+.class__title {
+  font-family: Montserrat;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+
+  color: #1A1A1A;
+  margin: 0;
+}
+
+.actions {
+  min-width: 120px;
+  display: flex;
+  align-items: center;
+}
+
+.actions a {
+  margin-left: 16px;
+}
+.actions a:first-child {
+  margin-left: 0;
+}
 </style>
