02e95721090cbcefa38bc0eb10ad87f1c32acb3e
oliveirabalsa <oliveirabalsa2@gmail.com>
2019-12-27 11:37:12 -0300

    correção do texto

--- pages/about.vue
+++ pages/about.vue
@@ -13,11 +13,11 @@
           SOBRE A NEW SCHOOL
         </h2>
         <p class="font-weight-bold subtext mr-3 ml-3">
-           A New School foi criado com intenção de ajudar os jovens de periferia que por muitas 
+           A New School foi criada com intenção de ajudar os jovens de periferia que por muitas 
            das vezes não conseguem ter uma educação 
            de qualidade. Com isso o seu fundador JP (João Malara) teve a brilhante ideia de 
            criar esse projeto sagrado. Com isso, a New School tem o objetivo de trazer o ensino de qualidade em uma linguagem simples
-            para o público jovem, que estuda, como também os que não estão estudam mais o ensino escolar.
+            para o público jovem, que estuda, como também os que não estudam mais o ensino escolar.
         </p>
       </v-flex>
 
