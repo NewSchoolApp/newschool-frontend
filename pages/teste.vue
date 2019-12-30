d4d8dca4204e352d5c1c2edbbdeea5813530cff4
Jonathan Pauluze <jonathanpauluze@gmail.com>
2019-12-30 08:19:05 -0300

    Página para testes do componente.

---
+++ pages/teste.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ShareBtn color="#CCC" iconColor="#FFF"/>
+  </div>
+</template>
+
+<script>
+import ShareBtn from '@/components/ShareBtn';
+export default {
+  components: {
+    ShareBtn
+  }
+}
+</script>
+
+<style>
+
+</style>
