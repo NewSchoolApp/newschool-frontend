17af309752686b2a674f46a2659a56c5e05a59ce
Guilherme Alonso <guilherme.malonso@gmail.com>
2019-12-30 23:45:48 -0300

    Função que faz a requisição de refresh token

--- services/http/auth.js
+++ services/http/auth.js
@@ -1,77 +1,119 @@
-import { http } from './config'
+import { http } from "./config";
 
 /**
  * @author Andrews
- * 
+ *
  * Serviço de auth do sistema
  */
 
 export default {
-
   /**
    * autenticação na API do sistema
    */
   login: (username, password) => {
-    let body = new FormData()
+    const body = new FormData();
 
-    let base64 = btoa(`${process.env.credentials.name}:${process.env.credentials.secret}`)
+    const base64 = btoa(
+      `${process.env.credentials.name}:${process.env.credentials.secret}`
+    );
 
-    let client_credentials = `Basic ${base64}`;
+    const client_credentials = `Basic ${base64}`;
 
     body.append("grant_type", "password");
     body.append("username", username);
-    body.append("password", password)
+    body.append("password", password);
 
-    return http.post("/oauth/token",
-      body, { headers: { 'Authorization': client_credentials } })
+    return http
+      .post("/oauth/token", body, {
+        headers: { Authorization: client_credentials }
+      })
       .then(res => {
         // armazenando tokens no storage
-        let auth = {
+        const auth = {
           accessToken: `Bearer ${res.data.accessToken}`,
           refreshToken: res.data.refreshToken
-        }
-        localStorage.setItem("auth", JSON.stringify(auth))
-      })
+        };
+        localStorage.setItem("auth", JSON.stringify(auth));
+      });
   },
 
-  getExternalCredentials: () => {    
+  getExternalCredentials: () => {
+    const base64 = btoa(
+      `${process.env.credentials.external.name}:${process.env.credentials.external.secret}`
+    );
 
-    let base64 = btoa(`${process.env.credentials.external.name}:${process.env.credentials.external.secret}`)
+    const client_credentials = `Basic ${base64}`;
+    const body = { grant_type: "client_credentials" };
 
-    let client_credentials = `Basic ${base64}`;
-    let body = { grant_type: "client_credentials" };
-
-    return http.post("oauth/token",
-      body, { headers: { 'Authorization': client_credentials } })
+    return http.post("oauth/token", body, {
+      headers: { Authorization: client_credentials }
+    });
   },
 
   signUp: (form, token) => {
-    return http.post("api/v1/user", form, { headers: { 'Authorization': `Bearer ${token}` } })
+    return http.post("api/v1/user", form, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
   },
 
   /**
    * Método para salvar as informações do usuário no local storage
-   * 
-   * TODO - Ramificar em um serviço específico de usuário. 
+   *
+   * TODO - Ramificar em um serviço específico de usuário.
    */
   getInfoUser: () => {
-    let auth = JSON.parse(localStorage.getItem("auth"));
+    const auth = JSON.parse(localStorage.getItem("auth"));
 
     if (auth) {
-      return http.get("/api/v1/user/me", { headers: { 'Authorization': auth.accessToken } })
-        .then(
-          res => {
-            let user = {
-              name: res.data.name || "Anônimo",
-              type : res.data.type || "Visitante"
-            }
-            localStorage.setItem("user", JSON.stringify(user));
-          }
-        )
-    }
-    else {
+      return http
+        .get("/api/v1/user/me", {
+          headers: { Authorization: auth.accessToken }
+        })
+        .then(res => {
+          const user = {
+            name: res.data.name || "Anônimo",
+            type: res.data.type || "Visitante"
+          };
+          localStorage.setItem("user", JSON.stringify(user));
+        });
+    } else {
       $nuxt._router.push("/login");
     }
+  },
 
+  refreshToken: () => {
+    const auth = JSON.parse(localStorage.getItem("auth"));
+
+    if (auth) {
+      const { refreshToken } = auth;
+
+      const body = new FormData();
+
+      const base64 = btoa(
+        `${process.env.credentials.name}:${process.env.credentials.secret}`
+      );
+
+      const clientCredentials = `Basic ${base64}`;
+
+      body.append("grant_type", "refresh_token");
+      body.append("refresh_token", refreshToken);
+
+      return http
+        .post("/oauth/token", body, {
+          headers: { Authorization: clientCredentials }
+        })
+        .then(res => {
+          const auth = {
+            accessToken: `Bearer ${res.data.accessToken}`,
+            refreshToken
+          };
+          localStorage.setItem("auth", JSON.stringify(auth));
+        })
+        .catch(() => {
+          $nuxt._router.push("/login");
+        });
+    } else {
+      $nuxt._router.push("/login");
+    }
   }
-}
+};
