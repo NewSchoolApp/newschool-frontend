350ff2add28141e525ee15ede1347e237cadcec5
Petherson Moreno <pethermoreno@gmail.com>
2019-12-31 23:18:48 -0300

    feat: created course page to student with empty body

---
+++ pages/student/course.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div id="page">
+      <!-- HTML aqui! -->
+      <p>course page</p>
+    </div>
+    <client-only>
+      <navigation-bar />
+    </client-only>
+  </div>
+</template>
+
+<script>
+import NavigationBar from '~/components/NavigationBar.vue'
+
+export default {
+  components: {
+    NavigationBar
+  }
+}
+</script>
+
+<style>
+
+</style>
