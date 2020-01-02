3030c3ed934958c062f573a8d02cfa54f02b0f9e
Petherson Moreno <pethermoreno@gmail.com>
2020-01-02 00:53:43 -0300

    feat: updated course page  - configured course page to show not found page if course doesn't exist  - added course info when found the course

--- pages/student/course.vue
+++ pages/student/course.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
+  <div v-if="loading">
+    <client-only>
+      <navigation-bar />
+    </client-only>
+  </div>
+  <not-found v-else-if="notFound" />
+  <div v-else-if="!notFound">
     <div id="page">
       <!-- HTML aqui! -->
-      <p>course page</p>
+      <p>Course {{ course.title }}</p>
+      <p>id: {{ course.id }}</p>
+      <p>authorId: {{ course.authorId }}</p>
+      <p>description: {{ course.description }}</p>
+      <p>slug: {{ course.slug }}</p>
+      <p>thumbUrl: {{ course.thumbUrl }}</p>
     </div>
     <client-only>
       <navigation-bar />
@@ -11,21 +22,39 @@
 </template>
 
 <script>
+import NotFound from '../public/404.vue'
 import NavigationBar from '~/components/NavigationBar.vue'
 import courses from '~/services/http/courses'
 
 export default {
-  mounted () {
-    courses.getBySlug('hook-lint-jest')
-      .then(({data}) => {
-        const {id, authorId, description, slug, thumbUrl, title } = data;
-        console.log('data: ', data)
-      })
-      // eslint-disable-next-line no-console
-      .catch(console.error)
-  },
   components: {
-    NavigationBar
+    NavigationBar,
+    NotFound
+  },
+  data () {
+    return {
+      loading: true,
+      notFound: false,
+      course: null
+    }
+  },
+  mounted () {
+    const { slug } = this.$route.params
+    courses.getBySlug(slug)
+      .then(({ data }) => {
+        const { id, authorId, description, slug, thumbUrl, title } = data
+        this.course = { id, authorId, description, slug, thumbUrl, title }
+        this.loading = false
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 404) {
+          this.notFound = true
+          this.loading = false
+          return
+        }
+        // eslint-disable-next-line no-console
+        console.error(error)
+      })
   }
 }
 </script>
