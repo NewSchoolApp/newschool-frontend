998230aff52e0b1da4e39bb65e0a36f5e1269ea5
guimalonso <guilherme.malonso@gmail.com>
2020-01-02 22:42:33 -0300

    Alteração no conteúdo do parâmetro refreshToken para chamada ao backend

--- services/http/auth.js
+++ services/http/auth.js
@@ -1,5 +1,5 @@
 import { ms } from 'ms'
-import { http } from "./config"
+import { http } from './config'
 
 /**
  * @author Andrews
@@ -12,50 +12,50 @@ export default {
    * autenticação na API do sistema
    */
   login: (username, password) => {
-    const body = new FormData();
+    const body = new FormData()
 
     const base64 = btoa(
-      `${process.env.credentials.name}:${process.env.credentials.secret}`
-    );
+      `${process.env.credentials.name}:${process.env.credentials.secret}`,
+    )
 
-    const client_credentials = `Basic ${base64}`;
+    const clientCredentials = `Basic ${base64}`
 
-    body.append("grant_type", "password");
-    body.append("username", username);
-    body.append("password", password);
+    body.append('grant_type', 'password')
+    body.append('username', username)
+    body.append('password', password)
 
     return http
-      .post("/oauth/token", body, {
-        headers: { Authorization: client_credentials }
+      .post('/oauth/token', body, {
+        headers: { Authorization: clientCredentials },
       })
       .then(res => {
         // armazenando tokens no storage
         const auth = {
           accessToken: `Bearer ${res.data.accessToken}`,
-          refreshToken: `Bearer ${res.data.refreshToken}`,
-          tokenExpiresIn: Date.now() + ms(res.data.expiresIn)
-        };
-        localStorage.setItem("auth", JSON.stringify(auth));
-      });
+          refreshToken: res.data.refreshToken,
+          tokenExpiresIn: Date.now() + ms(res.data.expiresIn),
+        }
+        localStorage.setItem('auth', JSON.stringify(auth))
+      })
   },
 
   getExternalCredentials: () => {
     const base64 = btoa(
-      `${process.env.credentials.external.name}:${process.env.credentials.external.secret}`
-    );
+      `${process.env.credentials.external.name}:${process.env.credentials.external.secret}`,
+    )
 
-    const client_credentials = `Basic ${base64}`;
-    const body = { grant_type: "client_credentials" };
+    const clientCredentials = `Basic ${base64}`
+    const body = { grant_type: 'client_credentials' }
 
-    return http.post("oauth/token", body, {
-      headers: { Authorization: client_credentials }
-    });
+    return http.post('oauth/token', body, {
+      headers: { Authorization: clientCredentials },
+    })
   },
 
   signUp: (form, token) => {
-    return http.post("api/v1/user", form, {
-      headers: { Authorization: `Bearer ${token}` }
-    });
+    return http.post('api/v1/user', form, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
   },
 
   /**
@@ -64,66 +64,69 @@ export default {
    * TODO - Ramificar em um serviço específico de usuário.
    */
   getInfoUser: () => {
-    const auth = JSON.parse(localStorage.getItem("auth"));
+    const auth = JSON.parse(localStorage.getItem('auth'))
 
     if (auth) {
       return http
-        .get("/api/v1/user/me", {
-          headers: { Authorization: auth.accessToken }
+        .get('/api/v1/user/me', {
+          headers: { Authorization: auth.accessToken },
         })
         .then(res => {
           const user = {
-            name: res.data.name || "Anônimo",
-            type: res.data.type || "Visitante"
-          };
-          localStorage.setItem("user", JSON.stringify(user));
-        });
+            name: res.data.name || 'Anônimo',
+            type: res.data.type || 'Visitante',
+          }
+          localStorage.setItem('user', JSON.stringify(user))
+        })
     } else {
-      $nuxt._router.push("/login");
+      // eslint-disable-next-line no-undef
+      $nuxt._router.push('/login')
     }
   },
 
   getNewAccessToken: refreshToken => {
-    const body = new FormData();
+    const body = new FormData()
 
     const base64 = btoa(
       `${process.env.credentials.name}:${process.env.credentials.secret}`,
-    );
+    )
 
-    const clientCredentials = `Basic ${base64}`;
+    const clientCredentials = `Basic ${base64}`
 
-    body.append("grant_type", "refresh_token");
-    body.append("refresh_token", refreshToken);
+    body.append('grant_type', 'refresh_token')
+    body.append('refresh_token', refreshToken)
 
     return http
-      .post("/oauth/token", body, {
-        headers: { Authorization: clientCredentials }
+      .post('/oauth/token', body, {
+        headers: { Authorization: clientCredentials },
       })
       .then(res => {
         const auth = {
           accessToken: `Bearer ${res.data.accessToken}`,
-          refreshToken: `Bearer ${res.data.refreshToken}`,
-          tokenExpiresIn: Date.now() + ms(res.data.expiresIn)
-        };
-        localStorage.setItem("auth", JSON.stringify(auth));
+          refreshToken: res.data.refreshToken,
+          tokenExpiresIn: Date.now() + ms(res.data.expiresIn),
+        }
+        localStorage.setItem('auth', JSON.stringify(auth))
       })
       .catch(() => {
-        $nuxt._router.push("/login");
-      });
+        // eslint-disable-next-line no-undef
+        $nuxt._router.push('/login')
+      })
   },
 
   isTokenValid: () => {
-    const auth = JSON.parse(localStorage.getItem("auth"));
+    const auth = JSON.parse(localStorage.getItem('auth'))
 
     if (auth) {
-      const { refreshToken, tokenExpiresIn } = auth;
-      const currentTime = Date.now();
+      const { refreshToken, tokenExpiresIn } = auth
+      const currentTime = Date.now()
 
       if (currentTime > tokenExpiresIn) {
-        this.getNewAccessToken(refreshToken);
+        this.getNewAccessToken(refreshToken)
       }
     } else {
-      $nuxt._router.push("/login");
+      // eslint-disable-next-line no-undef
+      $nuxt._router.push('/login')
     }
-  }
-};
+  },
+}
