ffea5202a2b29701b7ec082fdf0e27a5ee3e0368
oliveirabalsa <oliveirabalsa2@gmail.com>
2020-01-05 23:30:11 -0300

    Sincronizando nova home

---
+++ pages/student/home.vue
@@ -0,0 +1,159 @@
+<template>
+  <main class="max-content" id="page">
+    <header class="welcome">
+      <h1 class="welcome-title">{{'Olá ' + user.name}}</h1>
+      <h2 class="welcome-subtitle">Seja bem vindo</h2>
+    </header>
+
+    <h3 class="title-section">CURSOS</h3>
+    <article class="article-container">
+      <course-card
+        :key="course.id"
+        v-for="course in courses"
+        :title="course.title"
+        :teacher="course.authorId"
+        :image="course.thumbUrl"
+        :slug="course.slug"
+      />
+    </article>
+    <client-only>
+      <navigation-bar />
+    </client-only>
+  </main>
+</template>
+
+<router>
+  {
+    path: '/aluno/home'
+  }
+</router>
+
+<script>
+import NavigationBar from "~/components/NavigationBar.vue";
+import CourseCard from "~/components/CourseCard";
+import courses from '~/services/http/courses';
+
+export default {
+  components: {
+    NavigationBar,
+    CourseCard
+  },
+  data: () => ({
+    user: {
+      name: ''
+    }
+  }),
+  computed: {
+    courses() {
+      return this.$store.state.courses.list
+    }
+  },
+  methods: {
+    loadUserName() {
+      let storedUser = JSON.parse(localStorage.getItem("user"))
+      if (storedUser) {
+        this.user = storedUser;
+        this.user.name = this.user.name.split(' ')[0]
+      }
+    }
+  },
+  mounted() {
+    this.loadUserName();
+  },
+  asyncData({ store, data, params, $axios }) {
+    return courses.getAll().then(response =>
+      store.commit('courses/set', response.data)
+    )
+  }
+};
+</script>
+
+<style scoped>
+.welcome{
+  padding: 1em 0 0 1.5em;
+}
+
+.welcome-title{
+  font-weight: 900;
+  font-size: 20px;
+  line-height: 24px;
+  margin-bottom: 4px;
+  color: #1A1A1A;
+}
+
+.welcome-subtitle{
+  font-weight: 900;
+  font-size: 16px;
+  line-height: 20px;
+  color: #6600CC;
+}
+
+@media screen and (orientation: portrait) {
+  .article-container {
+    flex-flow: column wrap;
+  }
+
+  @media (min-width: 27.5em) {
+    .article-container {
+      flex-flow: row wrap;
+      padding: 0 1em 56px 1.5em;
+    }
+  }
+}
+
+@media screen and (orientation: landscape) {
+  .article-container {
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-content: flex-start;
+    overflow-x: auto;
+    margin: 0 2em 56px 1.5em;
+    padding: 0;
+  }
+
+  @media (min-width: 48em) {
+    .article-container {
+      flex-flow: row wrap;
+      margin: 0;
+    }
+  }
+}
+
+@media (min-width: 48em) {
+  .welcome {
+    padding-top: 2em;
+  }
+
+  .welcome-title {
+    font-size: 2em;
+    line-height: 0.8em;
+  }
+
+  .welcome-subtitle {
+    font-size: 1.3em;
+  }
+}
+
+.article-container {
+  display: flex;
+  max-width: 100vw;
+  padding: 0 2em 56px 1.5em;
+}
+
+.max-content{
+  max-height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 0 auto;
+}
+
+.title-section{
+  font-weight: 900;
+  line-height: 17px;
+  color: #6600CC;
+  padding: 1.25em 0 0.5em 1.5em;
+  text-transform: uppercase;
+}
+</style>
