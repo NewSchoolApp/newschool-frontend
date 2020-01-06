5fbc651bb97a7041f1d41706af52949b2a93b71f
oliveirabalsa <oliveirabalsa2@gmail.com>
2020-01-05 23:18:53 -0300

    adicionando scoped e overflow para correção de bug

--- pages/student/home.vue
+++ pages/student/home.vue
@@ -1,22 +1,21 @@
 <template>
-  <main class="max-content">
-      <header class="welcome">
-        <h1 class="welcome-title">Olá joão</h1>
-        <h2 class="welcome-subtitle">Seja bem vindo</h2>
-      </header>
+  <main class="max-content" id="page">
+    <header class="welcome">
+      <h1 class="welcome-title">{{'Olá ' + user.name}}</h1>
+      <h2 class="welcome-subtitle">Seja bem vindo</h2>
+    </header>
 
-      <article :key="category.name" v-for="category in categoryCourses">
-        <h3 class="titleSection">{{category.name}}</h3>
-        <horizontal-scroll :horizontalPadding="34" :gap="20">
-          <course-card
-            v-for="course in category.courses"
-            :key="course.id"
-            :title="course.name"
-            :teacher="course.teacher"
-            :image="course.image"
-          />
-        </horizontal-scroll>
-      </article>
+    <h3 class="title-section">CURSOS</h3>
+    <article class="article-container">
+      <course-card
+        :key="course.id"
+        v-for="course in courses"
+        :title="course.title"
+        :teacher="course.authorId"
+        :image="course.thumbUrl"
+        :slug="course.slug"
+      />
+    </article>
     <client-only>
       <navigation-bar />
     </client-only>
@@ -31,101 +30,47 @@
 
 <script>
 import NavigationBar from "~/components/NavigationBar.vue";
-import HorizontalScroll from "~/components/HorizontalScroll.vue";
 import CourseCard from "~/components/CourseCard";
+import courses from '~/services/http/courses';
 
 export default {
-  data: () => ({
-    categoryCourses:[{
-      name: "Cursos",
-      courses:[
-        {
-          id:1,
-          name: "Curso 1",
-          teacher: "Gustavo Raña",
-          image: "http://i.imgur.com/SrPdUD4.png"
-        },
-        {
-          id:2,
-          name: "Curso 2",
-          teacher: "Mateus Vinicius",
-          image: "https://s31450.pcdn.co/wp-content/uploads/2015/10/iStock_computer-art.151110.jpg"
-        },
-        {
-          id:3,
-          name: "Curso 3",
-          teacher: "Patricia Camarões",
-          image: "http://i.imgur.com/SrPdUD4.png"
-        },
-      ]
-    },
-    {
-      name: "Cursos top",
-      courses:[
-        {
-          id:4,
-          name: "Curso 1",
-          teacher: "Gustavo Raña",
-          image: "https://ugc.futurelearn.com/uploads/images/41/22/regular_4122fa44-6ac3-4c39-ba9b-29a7160e763b.jpg"
-        },
-        {
-          id:5,
-          name: "Curso 2",
-          teacher: "Mateus Vinicius",
-          image: "https://s31450.pcdn.co/wp-content/uploads/2015/10/iStock_computer-art.151110.jpg"
-        },
-        {
-          id:6,
-          name: "Curso 3",
-          teacher: "Patricia Camarões",
-          image: "http://i.imgur.com/SrPdUD4.png"
-        },
-      ]
-    },
-    {
-      name: "Cursos mais vistos",
-      courses:[
-        {
-          id:7,
-          name: "Curso 1",
-          teacher: "Gustavo Raña",
-          image: "https://s31450.pcdn.co/wp-content/uploads/2015/10/iStock_computer-art.151110.jpg"
-        },
-        {
-          id:8,
-          name: "Curso 2",
-          teacher: "Mateus Vinicius",
-          image: "https://ugc.futurelearn.com/uploads/images/41/22/regular_4122fa44-6ac3-4c39-ba9b-29a7160e763b.jpg"
-        },
-        {
-          id:9,
-          name: "Curso 3",
-          teacher: "Patricia Camarões",
-          image: "http://i.imgur.com/SrPdUD4.png"
-        },
-      ]
-    },
-    ]
-  }),
   components: {
     NavigationBar,
-    HorizontalScroll,
     CourseCard
+  },
+  data: () => ({
+    user: {
+      name: ''
+    }
+  }),
+  computed: {
+    courses() {
+      return this.$store.state.courses.list
+    }
+  },
+  methods: {
+    loadUserName() {
+      let storedUser = JSON.parse(localStorage.getItem("user"))
+      if (storedUser) {
+        this.user = storedUser;
+        this.user.name = this.user.name.split(' ')[0]
+      }
+    }
+  },
+  mounted() {
+    this.loadUserName();
+  },
+  asyncData({ store, data, params, $axios }) {
+    return courses.getAll().then(response =>
+      store.commit('courses/set', response.data)
+    )
   }
 };
 </script>
 
-<style>
-.max-content{
-  width: 100%;
-  max-width: 860px;
-  box-sizing: border-box;
-  margin: 0 auto;
-  padding-bottom: 80px;
-}
-
+<style scoped>
 .welcome{
-  padding: 16px 34px 0;
+  padding: 1em 0 0 1.5em;
 }
 
 .welcome-title{
@@ -143,13 +88,72 @@ export default {
   color: #6600CC;
 }
 
-.titleSection{
-  font-weight: 900;
-  font-size: 14px;
-  line-height: 17px;
-  color: #6600CC;
-  padding: 20px 34px 10px;
-  text-transform: uppercase;
+@media screen and (orientation: portrait) {
+  .article-container {
+    flex-flow: column wrap;
+  }
+
+  @media (min-width: 27.5em) {
+    .article-container {
+      flex-flow: row wrap;
+      padding: 0 1em 56px 1.5em;
+    }
+  }
 }
 
+@media screen and (orientation: landscape) {
+  .article-container {
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-content: flex-start;
+    overflow-x: auto;
+    margin: 0 2em 56px 1.5em;
+    padding: 0;
+  }
+
+  @media (min-width: 48em) {
+    .article-container {
+      flex-flow: row wrap;
+      margin: 0;
+    }
+  }
+}
+
+@media (min-width: 48em) {
+  .welcome {
+    padding-top: 2em;
+  }
+
+  .welcome-title {
+    font-size: 2em;
+    line-height: 0.8em;
+  }
+
+  .welcome-subtitle {
+    font-size: 1.3em;
+  }
+}
+
+.article-container {
+  display: flex;
+  max-width: 100vw;
+  padding: 0 2em 56px 1.5em;
+}
+
+.max-content{
+  max-height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 0 auto;
+}
+
+.title-section{
+  font-weight: 900;
+  line-height: 17px;
+  color: #6600CC;
+  padding: 1.25em 0 0.5em 1.5em;
+  text-transform: uppercase;
+}
 </style>
