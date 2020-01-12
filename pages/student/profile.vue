e40e2b685162f1ec4576ed443d1d5ee7b093997f
oliveirabalsa <oliveirabalsa2@gmail.com>
2020-01-12 20:57:14 -0300

    Correção da page 404

--- pages/student/profile.vue
+++ pages/student/profile.vue
@@ -5,11 +5,7 @@
         <v-col cols="6" md="8">
           <v-card class="mx-auto item" max-width="344" @click="gotoChangeData">
             <img src="~/assets/perfil.png" alt />
-<<<<<<< HEAD
             <a  href>Alterar Dados</a>
-=======
-            <a href>Alterar Dados</a>
->>>>>>> b0908a08dbd0a7c6f81a8972bb1a482f4331d987
           </v-card>
         </v-col>
 
