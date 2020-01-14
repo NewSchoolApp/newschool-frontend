2211a23e3db8379f777d91b51815c99841f01195
cidcair <cidcair@hotmail.com>
2020-01-14 11:38:02 -0300

    entrega da task 1.15

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,14 +40,12 @@ export default {
           'NEWSCHOOL@EXTERNALSECRET',
       },
     },
-    dateEnd: 
-    process.env.OPENING_DATE || '20/01/2020',
-
     endpoints: {
       CERTIFICATES_ME: 'api/v1/user/me/certificate',
       USER_ME: 'api/v1/user/me',
       LOGIN: 'oauth/token',
       SIGN_UP: 'api/v1/user/student',
+      FORGOT_PASSWORD: 'api/v1/user/forgot-password'
     },
     GATOKEN: process.env.GA_TOKEN,
   },
@@ -171,6 +169,6 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {},
+    extend(config, ctx) { },
   },
 }
