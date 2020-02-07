4b1ddf36cef4887c4170a9dbcc5e83f889afd70e
Petheson Moreno <pethermoreno@gmail.com>
2020-02-07 08:53:26 -0300

    refactor: change api to dev

--- nuxt.config.js
+++ nuxt.config.js
@@ -146,7 +146,7 @@ export default {
   env: {
     baseUrl:
       process.env.VUE_APP_BASE_URL ||
-     'https://newschoolbrapi-predev.herokuapp.com/',
+     'https://newschoolbrapi-dev.herokuapp.com/',
     
     credentials: {
       name: process.env.VUE_APP_CLIENT_CREDENTIAL_NAME || 'NEWSCHOOL@FRONT',
