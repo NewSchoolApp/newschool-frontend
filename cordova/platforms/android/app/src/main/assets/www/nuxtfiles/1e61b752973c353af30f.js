2c79b823fd1f4a6f566bbdd7c15429e74ad47741
Leonardo <oliveirabalsa2@gmail.com>
2020-11-07 02:44:29 -0300

    test: upload files

---
+++ cordova/platforms/android/app/src/main/assets/www/nuxtfiles/1e61b752973c353af30f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[56],{717:function(t,e,n){"use strict";n.r(e);var r={},l=n(35),component=Object(l.a)(r,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",{attrs:{id:"app"}},[e("router-view")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
