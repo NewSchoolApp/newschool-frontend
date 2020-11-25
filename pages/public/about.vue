f7e44ae0eab5e1efc627b51f63a2254a739ed5c8
cidcair <cidcair@hotmail.com>
2020-11-24 21:44:35 -0300

    colocar video no sobre

--- pages/public/about.vue
+++ pages/public/about.vue
@@ -34,6 +34,17 @@
               jovens como você encarem melhor os
               <span>desafios da vida.</span>
             </p>
+
+          </div>
+           <div class="video-iframe-container">
+            <iframe
+              width="350"
+              height="200"
+              :src="urlVideo.replace('watch?v=', 'embed/')"
+              frameborder="0"
+              allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </div>
         </v-flex>
       </v-layout>
@@ -50,12 +61,17 @@
 <script>
 import NavigationBar from '~/components/NavigationBar.vue';
 import HeaderBar from '~/components/Header.vue';
+import http from '~/services/http/generic';
 
 export default {
   components: {
     HeaderBar,
     NavigationBar,
   },
+   data: () => ({
+    urlVideo: 'https://www.youtube.com/watch?v=u4O8wE0gYO0',
+    loading: true,
+   }),
 };
 </script>
 <style scoped>
@@ -108,6 +124,23 @@ p ::v-deep .subtext p {
   ::v-deep .h1__theme {
     font-size: 1.2rem;
     text-align: center;
+  }
+}
+
+.video-iframe-container {
+  margin-top: 0.75em;
+  position: relative;
+  padding-bottom: 56.25%; /* 16:9 */
+  padding-top: 25px;
+  height: 0;
+
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
   }
 }
 </style>
