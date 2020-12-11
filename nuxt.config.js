54132347962a6b933a94a17113d5c3f0ad2f45b9
Leonardo <oliveirabalsa2@gmail.com>
2020-12-11 15:27:38 -0300

    feat: sync files

--- nuxt.config.js
+++ nuxt.config.js
@@ -158,9 +158,9 @@ export default {
   env: {
     domain: process.env.DOMAIN_URL || 'https://newschoolapp.com.br',
     baseUrl:
-      process.env.VUE_APP_BASE_URL || //'https://6870dc1db9e8.ngrok.io',
+      process.env.VUE_APP_BASE_URL || // 'https://6870dc1db9e8.ngrok.io',
       // 'http://newschool-api-dev2.eba-gxtzwa9m.us-east-2.elasticbeanstalk.com/',
-      'https://9ddlz0bte4.execute-api.us-east-2.amazonaws.com/dev',
+      'https://9ddlz0bte4.execute-api.us-east-2.amazonaws.com/prod',
     // http://develop.dev-newschool.tk/
     credentials: {
       name: process.env.VUE_APP_CLIENT_CREDENTIAL_NAME || 'NEWSCHOOL@FRONT',
